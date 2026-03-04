b06818aa720b96353c2b81f6320231103c9a4174
Gladwin George <gladwingeorge189@gmail.com>
2026-03-04 20:09:27 +0000

    Initialized React application with routing and Vercel Analytics.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 import './styles/base.css'
 import './styles/pages/home.css'
@@ -12,6 +13,7 @@ import './styles/responsive.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </React.StrictMode>
